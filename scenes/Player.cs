88490bc66493d2b6f2ec24ad3790d633dbe0f39d
Christopher Nguyen <chriswnguyen563@gmail.com>
2025-11-13 20:36:03 -0500

    Made a skeleton stucture for the character, player, and enemy claases.

---
+++ scenes/Player.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class Player : Character
+{
+	public void walk(Vector2 direction);
+	
+	public void add_move();
+	
+	// public List<Moves> getMoveList();
+}
