2d9dd1f9a27241b0a57977541bdba1624da604be
Christopher Nguyen <chriswnguyen563@gmail.com>
2025-11-13 20:36:03 -0500

    Made a skeleton stucture for the character, player, and enemy claases.

---
+++ scenes/Character.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Character : Area2D
+{
+	private:
+		Vector2 position;
+		int hp;
+	public:
+		float attack_multiplier;
+		float defense_multiplier;
+	
+	public void attack();
+	
+	public void defend();
+	
+	public float evaluate_effectiveness();
+	
+	public bool is_alive() {
+		return (hp <=0)? true: false;
+	}
+	
+	public void take_damage(float damage) {
+		hp -= damage;
+	}
+}
---
+++ scenes/Enemy.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class Enemy : Character
+{
+	GD.Print("Enemy Class");
+	// public Move selectRandomMove(); 
+}
