5e3fe6ac5f7c075ba835c0e953fc3e3d29a108fc
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2025-11-18 13:16:00 -0500

    update word manager to ignore case and store info in dictionaries

--- scripts/WordManager.cs
+++ scripts/WordManager.cs
@@ -3,59 +3,76 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 
 
-public class Word
+public record WordInfo(string Word, string[] Types, string[] Definitions)
 {
-    public Word(string word, List<string> types, List<string> definitions) : base()
-    {
-        this.word = word.ToLower();
-        this.types = types.Select(type => type.ToLower()).ToList();
-        this.definitions = definitions;
-    }
-
-    public string word { get; init; }
-    public List<string> types { get; init; }
-    public List<string> definitions { get; init; }
+    public string Word { get; init; } = Word.ToLower();
+    public string[] Types { get; init; } = [.. Types.Select(type => type.ToLower())];
 
     public override string ToString()
     {
-        var builder = new StringBuilder($"{nameof(Word)} {{ {nameof(word)} = {word}, ");
+        var builder = new StringBuilder($"{nameof(WordInfo)} {{ {nameof(Word)} = {Word}, ");
 
-        builder.Append($"{nameof(types)} = {{ {string.Join(", ", types)} }}, ");
+        builder.Append($"{nameof(Types)} = {{ {string.Join(", ", Types)} }}, ");
 
-        builder.Append($"{nameof(definitions)} = {{ {string.Join(", ", definitions)} }} ");
+        builder.Append($"{nameof(Definitions)} = {{ {string.Join(", ", Definitions)} }} ");
 
-        builder.Append("}");
+        builder.Append('}');
         return builder.ToString();
     }
 }
 
 public partial class WordManager : Node
 {
-    private List<Word> words;
-    public List<Word> Words => words;
+    static private HashSet<string> words;
+    static public HashSet<string> Words => words;
 
+    static private Dictionary<string, string[]> types;
+    static public Dictionary<string, string[]> Types => types;
 
+    static private Dictionary<string, string[]> definitions;
+    static public Dictionary<string, string[]> Definitions => definitions;
 
+    static private readonly JsonSerializerOptions jsonOptions = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        AllowTrailingCommas = true,
+        ReadCommentHandling = JsonCommentHandling.Skip,
+    };
+
+    public static string TitleCaseWord(string word) => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word);
     public override void _Ready()
     {
-        using (var wordsFile = FileAccess.Open("res://words/words.json", FileAccess.ModeFlags.Read))
-        {
-            words = JsonSerializer.Deserialize<List<Word>>(wordsFile.GetAsText());
-        }
+        using var wordsFile = FileAccess.Open("res://words/words.json", FileAccess.ModeFlags.Read);
+
+        var wordData = JsonSerializer.Deserialize<WordInfo[]>(wordsFile.GetAsText(), options: jsonOptions);
+
+        words = [.. wordData.Select(word => word.Word)];
+
+        types = wordData
+            .ToDictionary(
+                word => word.Word,
+                word => word.Types
+            );
+        definitions = wordData
+            .ToDictionary(
+                word => word.Word,
+                word => word.Definitions
+            );
+
 
         if (OS.IsDebugBuild())
         {
-            foreach (var word in words)
+            foreach (var word in wordData)
             {
                 GD.Print(word);
             }
         }
-
     }
 }
 
