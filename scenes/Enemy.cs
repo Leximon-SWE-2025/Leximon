df5e01e932d7e6a93c052ecf6c6d1d0dbd274625
Christopher Nguyen <chriswnguyen563@gmail.com>
2025-11-30 21:28:23 -0500

    made adjustments so it returns the move the enemy plays

--- scenes/Enemy.cs
+++ scenes/Enemy.cs
@@ -10,8 +10,9 @@ public partial class Enemy : Character
     {
         // random number from 0 to 1
         // 0 represents Attack, 1 represents Defend
-
-        return (rand.Next(0, 2) == 0) ? Attack : Defend;
+        int enemy_length = CurrentMoves.Length;
+        int move_no = (rand.Next(0, enemy_length));
+        return CurrentMoves[move_no];
     }
     public override void Attack(Move move)
     {
