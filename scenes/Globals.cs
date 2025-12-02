6cff60f2b6d209313d34d6e74c12e89044ccc31c
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2025-12-02 17:12:32 -0500

    add: path to save file

--- scenes/Globals.cs
+++ scenes/Globals.cs
@@ -1,8 +1,12 @@
+using Godot;
 using System;
+using System.IO;
 
 internal class Globals
 {
     public static readonly int TILE_SIZE = 16;
+
+    public static string SAVE_FILE_PATH => "user://savegame.save";
 
     public static TimeSpan MovementTime => TimeSpan.FromSeconds(0.15);
     public static double MovementTimeSec => MovementTime.TotalSeconds;
